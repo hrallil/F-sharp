907548850c9b94ccbb04672a314c52a3925a16d9
hrallil <mathias.larsen0@gmail.com>
2023-03-17 00:07:04 +0100

    finished Week2

--- week2/Program.fs
+++ week2/Program.fs
@@ -2,7 +2,7 @@
 
     //1. [Code-together] Modify the solution to exercise 2 from last week so that it detects when there are no solutions to the equation
     type roots = |Zero | One of float|Two of float*float
-    let supSolve (a:float,b:float,c:float) = 
+    let solve (a:float,b:float,c:float) = 
         let d = b**2 - 4.0*a*c
         if d < 0 then Zero 
         else if d = 0 then One (-b / 2.0*a)
@@ -28,9 +28,9 @@
     let fac n = loop n 1 
 
     //5. Implement a function count x ys that returns the number of times x occurs in the list ys. 
-    let rec count x = function  
+    let rec count5 x = function  
         | [] -> 0
-        | n::_l -> if n = x  then 1 + count x _l else count x _l
+        | n::_l -> if n = x  then 1 + count5 x _l else count5 x _l
 
     //> count 5 [1;5;6];; -> 1
 
@@ -46,7 +46,7 @@
 
     let rec countMatches f = function
         | [] -> 0
-        | n::_x -> if f n = true then 1 + countMatches f _x else countMatches f _x
+        | n::_x -> if f n then 1 + countMatches f _x else countMatches f _x
         
     //countMatches (fun x -> x<5)[1;3;6];; -> 2
     
@@ -70,11 +70,13 @@
     // sum f [1;3;2;5;-2;-5];; -> -7
 
 
-    //8. [Hand-in] Implement a function split such that
+    //8. [Hand-in] Implement a function split such that: split [x1; x2; x3; x4; ...] = ([x1; x3; ...], [x2; x4; ...])
 
-    let rec split1 = function
+    let rec splitOddEven = function
         | []-> ([],[])
-        | x::_l -> let (l1,l2) = split1 _l in if x % 2 = 0 then (x::l1,l2) else (l1,x::l2)
+        | x::_l -> let (l1,l2) = splitOddEven _l in if x % 2 = 0 then (x::l1,l2) else (l1,x::l2)
+
+    
 
 
     let rec split = function
@@ -82,6 +84,40 @@
         |[x] -> ([x],[])
         |x::y::_l -> let (l,r) = split _l in (x::l, y::r)
 
-    //9. 
+    //9. A list of integers [x1; x2; x3; ...; xn] is weakly ascending if  x1 ≤ x2 ≤ x3 ≤ ... ≤ xn. 
+    //a) Implement a function isWeaklyAscending xs that returns true if the list xs is weakly ascending (sorted), and false otherwise
 
+    let rec isWeaklyAscending = function 
+        | [] -> true
+        | [_] -> true
+        | x::y::_l -> if x<=y then isWeaklyAscending (y::_l) else false
 
+    //b) Implement a function count x ys that returns the number of times the integer x occurs in the weakly ascending list ys. 
+    let rec count x = function
+        | [] -> 0
+        | n::_l -> if n = x then 1 + count x _l else count x _l
+
+    // > count 5 [1;3;5;6;34;5;7];; -> 2
+    // > count 5 [1;5;5;6;5;5;7];; -> 4
+    // > count 5 [1;2;2;6;2;25;7];; -> 2
+
+    //c) Implement a function union xs ys that returns a weakly ascending list containing the elements that occur in either of the weakly ascending lists xs or ys (or both). For instance 
+    // EXAPLE:  > union [2; 3; 3; 5; 8] [1; 3; 3; 3; 4; 5] -> [1; 2; 3; 3; 3; 3; 3; 4; 5; 5; 8]
+
+    let rec union xs ys = 
+        match (xs,ys) with 
+            | (_,[]) -> xs
+            | ([],_) -> ys
+            | (x::_lx, y::_ly) -> if x<y then x::union _lx ys else y::union xs _ly
+
+    //10. [Hand-in] Notice that union takes two sorted lists as input and returns a sorted list containing all the elements of the two inputs. This operation is also called merging. 
+    let rec mergesort = function
+        | [] -> []
+        | [x] -> [x]
+        | xs -> let l,r = split xs in union (mergesort l)(mergesort r) 
+
+    //> mergesort [1;5;3;4;7;2;6;8;7;4;9];; -> [1; 2; 3; 4; 4; 5; 6; 7; 7; 8; 9]
+    //> mergesort [] -> []
+    //> mergesort [1] -> [1]
+    //> mergesort [1;1] -> [1;1]
+    //> mergesort [5;4;3;2;1] -> [1;2;3;4;5]
